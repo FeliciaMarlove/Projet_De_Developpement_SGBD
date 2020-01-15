93d47b6d8980141dab089862eedaebe2b039e17c
FeliciaMarlove <missflorencemary@hotmail.com>
2020-01-15 20:02:43 +0100

    Initial commit.

---
+++ src/main/java/be/iramps/florencemary/devsgbd/DevsgbdApplication.java
@@ -0,0 +1,13 @@
+package be.iramps.florencemary.devsgbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsgbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsgbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/iramps/florencemary/devsgbd/DevsgbdApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iramps.florencemary.devsgbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsgbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
